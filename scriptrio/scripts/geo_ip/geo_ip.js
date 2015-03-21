3653f8079e5f2c39af509071897a6fcf1fcff657
Chopivy <zbigniew.mlynarski@gmail.com>
2015-03-21 19:32:07 +0100

    Update geo_ip.js

--- scriptrio/scripts/geo_ip/geo_ip.js
+++ scriptrio/scripts/geo_ip/geo_ip.js
@@ -1,1 +1,24 @@
-
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var http = require("http");
+var ip = request.headers["x-forwarded-for"];
+var iplookupUrl = "http://www.telize.com/geoip/" + ip;
+var response = http.request({url: iplookupUrl});
+if(response.status == "200"){
+    var result = JSON.parse(response.body);
+    var country = result.country;
+    if(!storage.local[country]){
+	 	storage.local[country] = {count :1}
+	}else{
+		storage.local[country].count += 1;
+	}
+	var message= "Thank you for tweeting from scriptr.io. There are now " + 
+	storage.local[country].count +  " tweets from " + country +".";
+	var tweetResult = tweet(message);
+	return "You've just tweeted [" + message + "] from scriptr.io, you can check it <a href='http://twitter.com/scriptrdem0' target='_blank'>here</a>."
+}else{
+	return "Ooops, we can't figure out your location!";
+}   							
