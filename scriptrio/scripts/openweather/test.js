ad1f57ffcb47852368987dcf6003a3a1d9f3bf24
Chopivy <zbigniew.mlynarski@gmail.com>
2015-03-21 21:04:22 +0100

    Tests on Node-RED : OK

--- scriptrio/scripts/openweather/test.js
+++ scriptrio/scripts/openweather/test.js
@@ -1,10 +1,15 @@
 /** Script ACLs do not delete 
  read=nobody 
 write=nobody
-execute=authenticated 
+execute=anonymous 
   **/ 
  // require the "http" module
 var http = require("http");
 // After requiring the module "http" you can use its function "request"
-var response = http.request({"url": "http://api.openweathermap.org/data/2.5/weather","params": {"":"lat=52.171&lon=22.29"}});
-return response;   							
+var weatherService = "http://api.openweathermap.org/data/2.5/weather"
+// invoke the weatherService third party API
+var callResult = http.request({"url":weatherService, "params": {"q":"siedlce,pl"}});
+// parse the result of the call using regular JSON object
+var weatherInfo = JSON.parse(callResult.body);
+var temperature = weatherInfo.main.temp -273.15;
+return Math.round(temperature).toString();   							
