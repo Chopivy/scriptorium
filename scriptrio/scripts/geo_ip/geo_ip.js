2e33fdb00fe50379dd2d5fafa1d9acdf84a035d2
Chopivy <zbigniew.mlynarski@gmail.com>
2015-03-22 11:19:35 +0100

    Checks & comments.

--- scriptrio/scripts/geo_ip/geo_ip.js
+++ scriptrio/scripts/geo_ip/geo_ip.js
@@ -7,9 +7,14 @@ execute=anonymous
 var ip = request.headers["x-forwarded-for"];
 var iplookupUrl = "http://www.telize.com/geoip/" + ip;
 var response = http.request({url: iplookupUrl});
-if(response.status == "200"){
-    var result = JSON.parse(response.body);
-    var lat = result.latitude;
-    var lon = result.longitude;
+//get the response of the body as a string
+var responseBodyStr = response.body;
+if (response.status == "200"){
+  var result = JSON.parse(responseBodyStr);
+  var lat = result.latitude;
+  var lon = result.longitude;
+  return {"lat" : lat, "lon" : lon};
+} else {
+  return "Remote API return an error " + response.status;
 }
-return {"lat" : lat, "lon" : lon};   							
+ 							   							
