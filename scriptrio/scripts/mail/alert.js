98858805c93d7af4972021dc62076ca2d60816fc
Chopivy <zbigniew.mlynarski@gmail.com>
2015-03-22 06:44:18 +0100

    First commit from scriptr.io

---
+++ scriptrio/scripts/mail/alert.js
@@ -0,0 +1,15 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=anonymous 
+  **/ 
+ var time = new Date();
+var mailBody = "Hello,<br>An intrusion was detected at " + time + "<br>";
+mailBody += "<a href='https://api.scriptr.io/discard?id=" + request.id + "'>Click here to discard the alert</a>";
+var emailConfig = {
+  "to": "zbigniew.mlynarski@gmail.com",
+  "fromName": "Node-RED flows",
+  "subject": "Intrusion alert",
+  "body": mailBody
+};
+return sendMail(emailConfig.to, emailConfig.fromName, emailConfig.subject, emailConfig.body);   							
